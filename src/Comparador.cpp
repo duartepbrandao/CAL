d055ad5e84bb9416a11e19c51deddb6ac14a8fac
Nelson <paracontas987@hotmail.com>
2014-05-29 00:21:59 +0100

    semelhante
    Já encontra as semelhanças

--- src/Comparador.cpp
+++ src/Comparador.cpp
@@ -7,51 +7,59 @@
 
 using namespace std;
 
+Comparador::Comparador() {}
 
-Comparador::Comparador(){}
+void Comparador::menuInicial() {
 
-void Comparador::menuInicial(){
-	cout << endl <<
-			"\t\t\t\t\t" << setfill('=') << setw(30) << "\n" <<
-			"\t\t\t\t\t" << "==Similaridade de Ficheiros==" << "\n" <<
-			"\t\t\t\t\t" << setfill('=') << setw(31) << "\n\n";
+	vector<string> vecOrig = sepLinhas("original.txt");
+	vector<string> vecCopia = sepLinhas("copia.txt");
 
-	for(int i=0; i<2; i++){
-		if(i==0) cout << "Ficheiro original: ";
-		else cout << "Ficheiro a comparar: ";
+	calcLCS(vecOrig,vecCopia);
+	backTrack(vecOrig, vecCopia, vecOrig.size(), vecCopia.size());
+	/*cout << endl << "\t\t\t\t\t" << setfill('=') << setw(30) << "\n"
+			<< "\t\t\t\t\t" << "==Similaridade de Ficheiros==" << "\n"
+			<< "\t\t\t\t\t" << setfill('=') << setw(31) << "\n\n";
+
+	for (int i = 0; i < 2; i++) {
+		if (i == 0)
+			cout << "Ficheiro original: ";
+		else
+			cout << "Ficheiro a comparar: ";
 
 		string nomeFicheiro;
 		getline(cin, nomeFicheiro);
 
 		ifstream existFile(nomeFicheiro.c_str());
-		if(existFile){
+		if (existFile) {
 			existFile.close();
-			if(i==0) this->original=nomeFicheiro.c_str();
-			else this->copia=nomeFicheiro.c_str();
-		}
-		else{
-			cout<<"\nEsse ficheiro não existe!\n";
+			if (i == 0)
+				this->original = nomeFicheiro.c_str();
+			else
+				this->copia = nomeFicheiro.c_str();
+		} else {
+			cout << "\nEsse ficheiro não existe!\n";
 			i--;
 		}
-	}
+	}*/
 	return;
 }
 
-void Comparador::menuPrincipal(){
-	bool display=true;
+void Comparador::menuPrincipal() {
+	bool display = true;
 	string choice; //Recebe o input de escolha
 
-	while(true){
-
+	while (true) {
 		while (true) {
-			if(display){
-				cout << "\t\t\t\t\t" << "#Ficheiro original: " << this->original << "\n" <<
-						"\t\t\t\t\t" << "#Ficheiro a comparar: " << this->copia << "\n";
-				//cout << "\t\t\t\t\t" << "1.Alterar Ficheiros a Comparar\n";
+			if (display) {
+				cout << "\t\t\t\t\t" << "#Ficheiro original: " << this->original
+						<< "\n" << "\t\t\t\t\t" << "#Ficheiro a comparar: "
+						<< this->copia << "\n";
 				cout << "\t\t\t\t\t" << "1.Visualizar Diferenças.\n";
 				cout << "\t\t\t\t\t" << "2.Quit\n\n";
-				display=false;
+				display = false;
 			}
+			this->copia = "copia.txt";
+			this->original = "original.txt";
 			cout << "Indique a opcao desejada: ";
 			getline(cin, choice);
 			if (choice.length() == 1 && (choice == "1" || choice == "2"))
@@ -60,73 +68,106 @@ void Comparador::menuPrincipal(){
 		}
 
 		int option = atoi(choice.c_str());
-		switch (option){
-		/*case 1:
-			alteraFicheiro();//alterar ficheiros a comparar
-			display=true;
-			break;*/
+		switch (option) {
 		case 1:
-			//visualizar diferencas
-			display=false;
+		{
+			vector<string> vecOrig = sepLinhas(this->original);
+			vector<string> vecCopia = sepLinhas(this->copia);
+			calcLCS(vecOrig,vecCopia);
+			backTrack(vecOrig, vecCopia, vecOrig.size(), vecCopia.size());
+			display = true;
 			break;
-		case 2:
-			display=false;
-			cout<<"O programa terminou.";
+		}
+		default: //case 2:
+			display = false;
+			cout << "O programa terminou.";
 			return;
 			break;
 		}
 	}
 }
 
-/*void Comparador::alteraFicheiro(){
-	bool exibir=true, terminar=false;
-	string choice = ""; //Recebe o input de escolha
+vector<string> Comparador::sepLinhas(string nome) {
+	vector<string> todasLinhas;
+	string linha;
 
-	while(!terminar){
-		while(true){
-			if(exibir){
-				cout << "\t\t\t\t\t" << "Alterar o ficheiro:\n" <<
-						"\t\t\t\t\t" << "1. Original\n" <<
-						"\t\t\t\t\t" << "2. Copia\n" <<
-						"\t\t\t\t\t" << "3. Voltar ao menu principal\n" <<
-						"Indique a opção desejada: ";
-				getline(cin, choice);
-				if (choice == "1" || choice == "2" || choice == "3")
-					break;
-				cout << "Opção invalida, insira uma opção novamente." << endl;
+	ifstream ficheiro(nome.c_str());
+	while (!ficheiro.eof()) {
+		getline(ficheiro, linha);
+		todasLinhas.push_back(linha);
+	}
+	ficheiro.close();
+	return todasLinhas;
+}
+
+vector<string> Comparador::sepFrases(string nome) {
+	vector<string> todasFrases;
+	string linha, frase;
+
+	ifstream ficheiro(nome.c_str());
+	while (!ficheiro.eof()) {
+		getline(ficheiro, linha);
+
+		unsigned pos = 0;
+		while (pos < linha.size()) {
+			pos = linha.find_first_of('.');
+			if (pos < linha.size()) {
+				frase += linha.substr(pos);
+				linha = linha.substr(pos + 1, linha.size());
+				todasFrases.push_back(frase);
+				frase.clear();
 			}
+			frase += linha;
 		}
+	}
+	todasFrases.push_back(frase); //pode-se ignorar caso o texto termine com um ponto final
+	ficheiro.close();
+	return todasFrases;
+}
 
-		switch (atoi(choice.c_str())){
-		case 1:
-		{
-			string nomeFicheiro = "";
-			cout << "Novo ficheiro original: ";
-			getline(cin, nomeFicheiro);
-			ifstream existFile(nomeFicheiro.c_str());
-			if(existFile){
-				existFile.close();
-				this->original=nomeFicheiro.c_str();
-			} else cout<<"Esse ficheiro não existe!\n\n";
-			break;
-		}
-		case 2:
-		{
-			string nomeFicheiro = "";
-			cout << "Novo ficheiro a comparar:";
-			getline(cin, nomeFicheiro);
-			ifstream existFile(nomeFicheiro.c_str());
-			if(existFile) {
-				existFile.close();
-				this->copia=nomeFicheiro.c_str();
-			} else cout<<"Esse ficheiro não existe!\n\n";
-			break;
-		}
-		default: //case 3
-			terminar=true;
-			break;
+void Comparador::calcLCS(vector<string> original, vector<string> copia) {
+
+	matrix = new int*[original.size()+1];
+	for (unsigned i = 0; i <=original.size(); i++)
+		matrix[i] = new int[copia.size()+1];
+
+	for (unsigned i = 0; i <=original.size(); i++)
+		matrix[i][0] = 0;
+	for (unsigned j = 0; j <=copia.size(); j++)
+		matrix[0][j] = 0;
+
+
+	for (unsigned i = 1; i < original.size(); i++) {
+		for (unsigned j = 1; j < copia.size(); j++) {
+			if (original[i-1]==copia[j-1]) {
+				matrix[i][j] = matrix[i-1][j-1] +1;
+			} else {
+				matrix[i][j] = max(matrix[i][j-1], matrix[i-1][j]);
+			}
 		}
 	}
 
-	return;
-}*/
+}
+
+void Comparador::backTrack(vector<string> original, vector<string> copia, int i,
+		int j) {
+	if (i == 0 || j == 0) {
+		return;
+	} else if (original[i-1]==copia[j-1]) {
+		backTrack(original, copia, i-1, j-1);
+		lcs.push_back(original[i-1]);
+		return;
+	} else {
+		if( matrix[i][j-1] > matrix[i-1][j] ) {
+			return backTrack(original, copia, i, j-1);
+		}
+		else {
+			return backTrack(original, copia, i-1, j);
+		}
+	}
+}
+
+vector<string> Comparador::getLCS(){
+	return this->lcs;
+}
+
--- src/Comparador.h
+++ src/Comparador.h
@@ -2,17 +2,24 @@
 #define _COMPARADOR_
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Comparador{
 private:
 	string original, copia;
+	int** matrix;
+	vector <string> lcs;
 public:
 	Comparador();
 	void menuInicial();
 	void menuPrincipal();
-	void alteraFicheiro();
+	vector<string> sepFrases(string nome);
+	vector<string> sepLinhas(string nome);
+	void calcLCS(vector<string> original, vector<string> copia);
+	void backTrack(vector<string> original, vector<string> copia, int i, int j);
+	vector<string> getLCS();
 };
 
 #endif
